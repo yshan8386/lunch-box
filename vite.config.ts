b3040a692eea5cce7c849536b30219c4de09333a
yds <somlh1212@gmail.com>
2026-04-08 10:06:17 +0900

    프록시 연결 추가 코밋

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,13 @@ export default defineConfig({
   plugins: [react()
     , tailwindcss()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://lunch-box-be.onrender.com',  // FastAPI 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
